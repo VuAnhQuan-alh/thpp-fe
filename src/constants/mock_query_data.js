19b668f70899783ce0ff5b12370c8bd776e01a7e
jvm <hoangvt979@gmail.com>
2021-10-25 15:25:54 +0700

    refactor code

--- src/constants/mock_query_data.js
+++ src/constants/mock_query_data.js
@@ -16,6 +16,30 @@ const mockQueryUserSent = {
     "dataDetail": {}
 }
 
+
+// USE THIS MOCK (mockTransactionInfo) TO DEV UI
+const mockTransactionInfo = {
+    windowType: '0',
+    chanelType: '1',
+    ipAddress: '12.123.12.2.2',
+    customerName: 'Nguyen Van A',
+    phone: '0333333333',
+    orderInfo: '123456',
+    orderDate: '1234567890123',
+    amount: '90000',
+    serviceCode: '1',
+    serviceName: 'Tiêm chủng',
+    hospitalType: '1',
+    hospitalName: 'Bệnh viện True Hope',
+    detail: {
+        services: [
+            { serviceName: 'Vắc xin ABC', serviceCode: 'svCode1', count: '01', price: '300000', amount: '300000' },
+        ],
+        discountCode: 'xyz',
+        discountAmount: '100000',
+    },
+}
+
 // 
 // expDate
 // ipAddress
